a430a2f063d825127d7553edc7eca1a57d0f74f8
djhart <dalton-hart@uiowa.edu>
2018-10-15 12:49:29 -0500

    update mid semester

---
+++ 2018-fall/mit-ocw/python-intro-6.189/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# print('hello, world!')
+# x = '  |  |  \n'
+# y = '--------\n'
+
+#print(x,y,x,y,x, sep="")
+
+a = (3*5)/(2+3)
+b = (7+9)**.5 * 2
+c = (4-7)**3
+d = (-19+100)**.25
+e = 6%4
+
+f = (7+9+3)*4
+g = (7+9)+3*4
+
+# print(a,b,c,d,e,sep="\n")
+# print (f,g,sep="\n")
+
+firstname = input("Enter your first name?")
+lastname = input("Enter your last name?")
+print("Enter your date of birth")
+mo = input("Month?")
+day = input("Day?")
+year = input("Year?")
+
+print(firstname, lastname, "was born on", mo, day+",", year)
